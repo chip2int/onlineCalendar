e99b9544886b5f3b70aaa02ab466d492e1c3ebb3
Joseph Toney <jdtoney@gmail.com>
2013-12-17 12:13:10 -0800

    Working implementation - 2

--- request-handler.js
+++ request-handler.js
@@ -1,4 +1,20 @@
 var url = require("url");
+var mongoose = require('mongoose');
+
+var userNameDB;
+var diaryEntryDB;
+
+
+exports.initDB = function() {
+
+	var userNameSchema = ({id: Number, name: String});
+	var diaryEntrySchema = ({date: String,  entry: String, userId: {type: Number, ref : 'userNameDB'}}	);
+
+	userNameDB = mongoose.model('userNameDB', userNameSchema);
+	diaryEntryDB = mongoose.model('diaryEntryDB', diaryEntrySchema);
+	
+};
+
 var statusCode = 200;
 
 var headers = {
@@ -29,11 +45,6 @@ Users.prototype.getEntry = function(date) {
 };
 
 
-var sendResponse = function (request, obj, callback) {
-
-};
-
-
 var saveMessage = function(request, response){
 	var data = '';
 	request.on('data', function(chunk) {
@@ -60,6 +71,46 @@ var saveMessage = function(request, response){
 
 };
 
+var saveMessageToDB = function(request, response){
+	var data = '';
+	request.on('data', function(chunk) {
+		data += chunk;
+	});
+
+
+	request.on('end', function(){
+
+	  var user = request.url.split("/")[1];
+    statusCode = 201;
+    data = JSON.parse(data);
+    
+    // Check if the user exists
+		var query = userNameDB.find({name: user});
+		query.find(function(err, person) {
+
+			if (person.length === 0) {
+				//Person not found. Create a User;
+				var newUser = new userNameDB({id:1, name:user}).save();
+				var newMessage = new diaryEntryDB({date:data["date"], entry:data["entry"], userId:1}).save();			
+			}
+			else {
+				//Person exists;
+				var newMessage = new diaryEntryDB({date:data["date"], entry:data["entry"], userId:person[0].id}).save();
+				// var query = diaryEntryDB.find({date:'09/09/2013'}, function(err, persons){
+				// 	for (var i = 0; i <  persons.length; i++) {
+				// 		person = persons[i];
+				// 		console.log("person", person.date, person.entry, person.id)
+				// 	}
+				// });
+
+			}
+		});
+
+    response.writeHead(statusCode, headers);
+    response.end(JSON.stringify('OK'));
+  });
+
+};
 
 
 var sendMessages = function(request, response){
@@ -82,8 +133,45 @@ var sendMessages = function(request, response){
 		}
 	}	
 	response.writeHead(statusCode, headers);
-	console.log("message", message);
 	response.end(JSON.stringify(message));	
+	
+};
+
+var sendMessagesFromDB = function(request, response){
+	statusCode = 200;
+	var message ="";
+
+	var user = url.parse(request.url).path.split("/")[1];
+	var ent = 	JSON.parse(decodeURIComponent(url.parse(request.url).query));
+	entryDate = ent["date"];
+
+	var query = userNameDB.find({name: user}, function(err, person) {
+
+		if (person.length === 0) {
+			//Person not found. Error!
+			statusCode = 404;
+	    message = "User not found";
+		}
+		else {
+			diaryEntryDB.find({date:entryDate, userId:person[0].id}, function(err, entry) {
+
+				if (err) {
+					console.log("Err", err);
+				}
+				
+				if (entry.length ===0 ) {
+					message = "No entry found!";
+				}
+				else {
+					message = entry[0].entry
+				}
+				response.writeHead(statusCode, headers);
+				response.end(JSON.stringify(message));	
+			});
+		}
+		
+	});
+	
 	
 };
 
@@ -95,8 +183,8 @@ var sendOptions = function(request, response){
 
 
 var actionListener = {
-	'POST': saveMessage,
-	'GET': 	 sendMessages,
+	'POST': saveMessageToDB,
+	'GET': 	 sendMessagesFromDB,
 	'OPTIONS': sendOptions
 };
 
