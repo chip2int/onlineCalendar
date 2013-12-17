0908c1c00fec8c02c8e468e75b74a83edcea662c
Joseph Toney <jdtoney@gmail.com>
2013-12-17 12:13:10 -0800

    Working implementation - 2

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var http = require("http");
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/onlineDiary_db');
+
+
 var requestHandler = require("./request-handler.js");
 
 var port = 8082;
 var ip = "127.0.0.1";
 
+
+// Define Schema for Database
+//var objectId = mongoose.Schema.Types.ObjectId;
+requestHandler.initDB();
 var server = http.createServer(requestHandler.handleRequest);
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
